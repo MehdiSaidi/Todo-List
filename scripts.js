e9ea38c5e78d7e109a75be0c49b4cf0348a051fd
Mehdi Saidi <mehdisaidi879@gmail.com>
2020-06-30 21:16:06 -0400

    todo list basic functionalities done

---
+++ scripts.js
@@ -0,0 +1,38 @@
+//events
+const element = document.querySelector('#inputText')
+const button = document.querySelector('#submit')
+
+button.addEventListener('click', function () {
+    populate(element.value);
+})
+
+//functions
+function populate(element) {
+    const display = document.querySelector('#myTable');
+    let displayedElement = document.createTextNode(element);
+
+    let checkbox = document.createElement('input');
+
+    checkbox.type = "checkbox";
+    checkbox.name = "name";
+    checkbox.value = "value";
+    checkbox.id = "todoElements";
+
+    let label = document.createElement('label')
+    label.htmlFor = "id";
+    label.appendChild(displayedElement);
+
+    let tableRow = document.createElement('tr');
+    let tableData = document.createElement('td');
+
+
+
+
+    tableData.appendChild(checkbox);
+    tableData.appendChild(label);
+    tableRow.appendChild(tableData);
+
+
+
+    display.appendChild(tableRow);
+}
